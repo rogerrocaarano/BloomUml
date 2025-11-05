76fb74035dbababe6601997c8fb5ce8e94a5ebc3
Roger Roca Arano <git@rogerroca.me>
2025-11-05 16:37:54 +0000

    refactor: change Origin and Destination properties to string type for improved flexibility

--- DiagramPersistenceService/src/Core/Model/UmlMultiplicity.cs
+++ DiagramPersistenceService/src/Core/Model/UmlMultiplicity.cs
@@ -2,18 +2,27 @@ namespace Core.Model;
 
 public class UmlMultiplicity : IValueObject
 {
-    public int Origin { get; }
-    public int Destination { get; }
+    public string Origin { get; }
+    public string Destination { get; }
 
-    public UmlMultiplicity(int origin, int destination)
+    public UmlMultiplicity(string origin, string destination)
     {
         Origin = origin;
         Destination = destination;
     }
 
+    public static UmlMultiplicity OneToOne() => new("1", "1");
+
+    public static UmlMultiplicity OneToMany() => new("1", "*");
+
+    public static UmlMultiplicity ManyToOne() => new("*", "1");
+
+    public static UmlMultiplicity ManyToMany() => new("*", "*");
+
     public override bool Equals(object? obj)
     {
-        if (obj is not UmlMultiplicity other) return false;
+        if (obj is not UmlMultiplicity other)
+            return false;
         return Origin == other.Origin && Destination == other.Destination;
     }
 
