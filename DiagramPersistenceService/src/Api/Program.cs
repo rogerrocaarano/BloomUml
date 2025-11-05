51cba3eada3229f094b1cfff578efab3c0b04dde
Roger Roca Arano <git@rogerroca.me>
2025-11-05 07:22:33 +0000

    feat: update Api project to include FastEndpoints.Swagger for enhanced API documentation

--- DiagramPersistenceService/src/Api/Program.cs
+++ DiagramPersistenceService/src/Api/Program.cs
@@ -1,6 +1,19 @@
+using FastEndpoints.Swagger;
+
 var bld = WebApplication.CreateBuilder();
-bld.Services.AddFastEndpoints();
+bld.Services
+.AddFastEndpoints()
+.SwaggerDocument(o =>
+{
+    o.DocumentSettings = s =>
+    {
+        s.Title = "BloomUML: DiagramPersistenceService API";
+        s.Version = "v1";
+    };
+});
 
 var app = bld.Build();
-app.UseFastEndpoints();
+app
+.UseFastEndpoints()
+.UseSwaggerGen();
 app.Run();
