79d07505bfe4937b6f37c98327c9453585104f08
Roger Roca Arano <git@rogerroca.me>
2025-11-05 15:30:46 +0000

    docs: add docstrings for AggregateRoots

--- DiagramPersistenceService/src/Core/Model/UmlClass.cs
+++ DiagramPersistenceService/src/Core/Model/UmlClass.cs
@@ -2,14 +2,45 @@ using OpenDDD.Domain.Model.Base;
 
 namespace Core.Model;
 
+/// <summary>
+/// Represents a UML class in a diagram.
+/// </summary>
 public class UmlClass : AggregateRootBase<Guid>
 {
+    /// <summary>
+    /// Gets the ID of the diagram this class belongs to.
+    /// </summary>
     public Guid DiagramId { get; private set; }
+
+    /// <summary>
+    /// Gets the name of the UML class.
+    /// </summary>
     public string Name { get; private set; }
+
+    /// <summary>
+    /// Gets the position of the class in the diagram.
+    /// </summary>
     public UmlPosition Position { get; private set; }
+
+    /// <summary>
+    /// Gets the collection of attributes for this class.
+    /// </summary>
     public ICollection<UmlAttribute> Attributes { get; private set; }
+
+    /// <summary>
+    /// Gets the collection of methods for this class.
+    /// </summary>
     public ICollection<UmlMethod> Methods { get; private set; }
 
+    /// <summary>
+    /// Initializes a new instance of the UmlClass class.
+    /// </summary>
+    /// <param name="id">The unique identifier for the class.</param>
+    /// <param name="diagramId">The ID of the diagram this class belongs to.</param>
+    /// <param name="name">The name of the class.</param>
+    /// <param name="position">The position of the class in the diagram.</param>
+    /// <param name="umlAttributes">The collection of attributes.</param>
+    /// <param name="umlMethods">The collection of methods.</param>
     private UmlClass(
         Guid id,
         Guid diagramId,
@@ -27,6 +58,13 @@ public class UmlClass : AggregateRootBase<Guid>
         Methods = umlMethods;
     }
 
+    /// <summary>
+    /// Creates a new UML class with the specified parameters.
+    /// </summary>
+    /// <param name="diagramId">The ID of the diagram the class belongs to.</param>
+    /// <param name="name">The name of the class.</param>
+    /// <param name="position">The position of the class in the diagram.</param>
+    /// <returns>A new UmlClass instance.</returns>
     public static UmlClass Create(Guid diagramId, string name, UmlPosition position)
     {
         return new UmlClass(Guid.NewGuid(), diagramId, name, position, [], []);
