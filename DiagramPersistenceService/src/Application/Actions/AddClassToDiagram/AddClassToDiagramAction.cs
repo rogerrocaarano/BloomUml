df3f7b0a166699dde0117bc73d59a8e1bd89cb38
Roger Roca Arano <git@rogerroca.me>
2025-11-06 14:28:31 +0000

    feat: implement AddClassToDiagram functionality

---
+++ DiagramPersistenceService/src/Application/Actions/AddClassToDiagram/AddClassToDiagramAction.cs
@@ -0,0 +1,27 @@
+using DiagramPersistenceServiceApi.Domain.Model;
+using DiagramPersistenceServiceApi.Domain.Service;
+using OpenDDD.Application;
+
+namespace DiagramPersistenceServiceApi.Application.Actions.AddClassToDiagram;
+
+public class AddClassToDiagramAction : IAction<AddClassToDiagramCommand, UmlClass>
+{
+    private readonly IDiagramsDomainService _diagramsDomainService;
+
+    public AddClassToDiagramAction(IDiagramsDomainService diagramsDomainService)
+    {
+        _diagramsDomainService = diagramsDomainService;
+    }
+
+    public async Task<UmlClass> ExecuteAsync(AddClassToDiagramCommand command, CancellationToken ct)
+    {
+        var position = new UmlPosition(command.PositionX, command.PositionY);
+        var umlClass = await _diagramsDomainService.CreateClassAsync(
+            command.DiagramId,
+            command.ClassName,
+            position,
+            ct
+        );
+        return umlClass;
+    }
+}
